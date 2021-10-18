84a29463ae8525c09482d404c312423f52160cc2
Bryan Pakulski <BryanPakulski@outlook.com>
2021-10-19 02:02:43 +1100

    Created enumerations for expected data inputs

--- src/Handlers/DataEngine/DataEngine.cpp
+++ src/Handlers/DataEngine/DataEngine.cpp
@@ -6,13 +6,21 @@
 void DataEngine::loadData(std::string filePath) {
 
     try {
-        io::CSVReader<3> in(filePath);
-        in.read_header(io::ignore_extra_column, "volume", "size", "speed");
+        io::CSVReader<Q_COLUMN_COUNT> in(filePath);
+        in.read_header(
+			io::ignore_extra_column,
+			COLUMN_MAPPING[COLUMNS::OPEN_TIME],
+			COLUMN_MAPPING[COLUMNS::CLOSE_TIME],
+			COLUMN_MAPPING[COLUMNS::OPEN],
+			COLUMN_MAPPING[COLUMNS::CLOSE],
+			COLUMN_MAPPING[COLUMNS::HIGH],
+			COLUMN_MAPPING[COLUMNS::LOW],
+			COLUMN_MAPPING[COLUMNS::VOLUME]
+		);
 
-        std::string vendor; int size; double speed;
-
-        while(in.read_row(vendor, size, speed)){
-            // do stuff with the data
+        while(in.read_row(Q_openTime, Q_closeTime, Q_open, Q_close, Q_high, Q_low, Q_volume)){
+            QLogger::GetInstance().Info("Loading file: " + filePath);
+			// do stuff with the data
         }
     } catch (io::error::can_not_open_file) {
         ErrorHandler::GetInstance().setError("Unable to open file: " + filePath);
--- src/Handlers/DataEngine/DataEngineEnum.h
+++ src/Handlers/DataEngine/DataEngineEnum.h
@@ -1,3 +1,22 @@
 #pragma once
 
 static const int Q_COLUMN_COUNT = 7;
+enum COLUMNS {OPEN_TIME, CLOSE_TIME, OPEN, CLOSE, HIGH, LOW, VOLUME};
+
+static std::unordered_map<int, std::string> COLUMN_MAPPING({
+   {COLUMNS::OPEN_TIME, "OpenTime"},
+   {COLUMNS::CLOSE_TIME, "CloseTime"},
+   {COLUMNS::OPEN, "Open"},
+   {COLUMNS::CLOSE, "Close"},
+   {COLUMNS::HIGH, "High"},
+   {COLUMNS::LOW, "Low"},
+   {COLUMNS::VOLUME, "Volume"}
+});
+
+std::string Q_openTime;
+std::string Q_closeTime;
+double Q_open;
+double Q_close;
+double Q_high;
+double Q_low;
+double Q_volume;
